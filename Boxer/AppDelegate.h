56240ca07c30b9b003c73ae8c5e329f243ba6677
William Bajzek <williambajzek@gmail.com>
2013-03-07 20:28:56 -0600

    Initial commit

--- Boxer/AppDelegate.h
+++ Boxer/AppDelegate.h
@@ -11,5 +11,12 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *countdown;
+@property (weak) IBOutlet NSPopUpButton *amount;
+@property (weak) IBOutlet NSButton *goButton;
+@property BOOL running;
+@property (weak) NSTimer *timer;
+- (IBAction)go:(NSButton *)sender;
+- (IBAction)timeChanged:(id)sender;
 
 @end
--- Boxer/AppDelegate.m
+++ Boxer/AppDelegate.m
@@ -8,11 +8,83 @@
 
 #import "AppDelegate.h"
 
+@interface AppDeledate
+-(void)count:(NSDate *)end;
+@end
+
 @implementation AppDelegate
+
+@synthesize amount;
+@synthesize countdown;
+@synthesize running;
+@synthesize goButton;
+
+NSTimer *currentTimer;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    running = NO;
+    currentTimer = nil;
+}
+
+- (IBAction)go:(NSButton *)sender {
+    NSTimer *timer;
+    if (!running) {
+        if ([countdown.stringValue isEqualToString:@"00:00"]) {
+            NSDate *end = [NSDate dateWithTimeIntervalSinceNow:(60*[amount.titleOfSelectedItem integerValue])+1];
+            timer = [NSTimer timerWithTimeInterval:.5 target:self selector:@selector(count:) userInfo:end repeats:YES];
+        }
+        else {
+            int minutes = [[countdown.stringValue substringToIndex:3] intValue] ;
+            int seconds = [[countdown.stringValue substringFromIndex:3] intValue];
+            NSDate *end = [NSDate dateWithTimeIntervalSinceNow:(60*minutes) + seconds];
+            timer = [NSTimer timerWithTimeInterval:.5 target:self selector:@selector(count:) userInfo:end repeats:YES];
+        }
+        [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+        currentTimer = timer;
+        [timer fire];
+        running = YES;
+        sender.title = @"Stop";
+    }
+    else {
+        [currentTimer invalidate];
+        currentTimer = nil;
+        running = NO;
+        sender.title = @"Go!";
+    }
+}
+
+- (IBAction)timeChanged:(id)sender {
+    running = NO;
+    goButton.title = @"Go!";
+    NSString *minutes = [self lpad:amount.titleOfSelectedItem ];
+    NSString *seconds = @"00";
+    countdown.stringValue = [NSString stringWithFormat:@"%@:%@",minutes,seconds];
+    if (currentTimer != nil)
+        [currentTimer invalidate];
+}
+
+-(void)count:(NSTimer *)timer {
+    NSDate *end = timer.userInfo;
+    int time = [end timeIntervalSinceNow];
+    NSString *minutes = [self lpad:[NSString stringWithFormat:@"%d",time/60]];
+    NSString *seconds = [self lpad:[NSString stringWithFormat:@"%d",time%60]];
+    
+    [countdown setStringValue:[NSString stringWithFormat:@"%@:%@",minutes,seconds]];
+    if (time == 0) {
+        [timer invalidate];
+        currentTimer = nil;
+        running = NO;
+        goButton.title = @"Go!";
+        NSBeep();
+    }
+}
+
+-(NSString *)lpad:(NSString *)input {
+    if (input.length == 1)
+        return [NSString stringWithFormat:@"0%@",input ];
+    else
+        return input;
 }
 
 @end
